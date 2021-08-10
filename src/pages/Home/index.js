87c08282c61ca3f903b04f80cab721edeab2f8b7
Aline Almeida <alinemelo.cavalcanti@gmail.com>
2021-08-10 13:50:54 -0300

    Aula 08 - Salvando dados no Storage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,24 @@ export default function Home(props) {
   const [ usuario, setUsuario ] = useState('');
   
   function handlePesquisaUsuario() {
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then( response => console.log(response.data));
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then( response => {
+      let oUsersData = response.data;
+      let repoUsersName = [];
+
+      //Mapeando apenas os nomes dos usuários e armazenando no objeto
+      oUsersData.map( (user) => {
+        repoUsersName.push(user.name);
+      });
+
+      //Convertendo o objeto em String
+      let aUsersData = JSON.stringify(oUsersData);
+
+      //Armazenando a string de nomes na memoria do navegador (local storage) **** Mesma ideia de JSON Model, só que armazena no navegador (memória temporária)
+      localStorage.setItem('usersData', aUsersData); 
+
+      //Para verificar se foi armazenado na memória do navegador.
+      // Abrir a janela de desenvolvedor (debugger), na opção 'Application > Storage > Local Storage'.
+    });
   }
 
   return (
