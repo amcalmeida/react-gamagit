d392611cf697cef98eb233bc67302d5a37cc6f3f
Aline Almeida <alinemelo.cavalcanti@gmail.com>
2021-08-10 19:25:33 -0300

    Aula 09 - Renderizando repositórios e programação declarativa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,43 @@
 import React, { useState } from 'react';
-
 import axios from 'axios';
+import { useHistory } from 'react-router-dom'
 
 import * as S from './styled';
 
 export default function Home(props) {
   const [ usuario, setUsuario ] = useState('');
+
+  const history = useHistory();
   
   function handlePesquisaUsuario() {
     axios.get(`https://api.github.com/users/${usuario}/repos`).then( response => {
-      let oUsersData = response.data;
-      let repoUsersName = [];
+      let oData = response.data;
+
+      let repositoriesName = [];
 
       //Mapeando apenas os nomes dos usuários e armazenando no objeto
-      oUsersData.map( (user) => {
-        repoUsersName.push(user.name);
+      oData.map( (repository) => {
+        repositoriesName.push(repository.name);
       });
 
       //Convertendo o objeto em String
-      let aUsersData = JSON.stringify(oUsersData);
+      let aData = JSON.stringify(repositoriesName);
+      console.log(repositoriesName);
+      console.log(aData);
 
       //Armazenando a string de nomes na memoria do navegador (local storage) **** Mesma ideia de JSON Model, só que armazena no navegador (memória temporária)
-      localStorage.setItem('usersData', aUsersData); 
-
+      localStorage.setItem('data', aData); 
       //Para verificar se foi armazenado na memória do navegador.
       // Abrir a janela de desenvolvedor (debugger), na opção 'Application > Storage > Local Storage'.
+
+      //Armazenando no histórico do navegador
+      history.push('/repositories');
     });
   }
 
   return (
     <S.Container>
-      <S.Input id="inputUsuario" name="inputUsuario" placeholder="Digite um usuário" value={usuario} className="inputUsuario" onChange={ e => setUsuario(console.log(e.target.value))} />
+      <S.Input id="inputUsuario" name="inputUsuario" placeholder="Digite um usuário" value={usuario} className="inputUsuario" onChange={ e => setUsuario(e.target.value)} />
       <S.Button type="button" onClick={handlePesquisaUsuario} >Pesquisar</S.Button>
     </S.Container>
   );
--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import * as S from './styled';
 
 export default function Repositories() {
+    const [ repositories, setRepositories ] = useState([]);
+
+    // useEffect monitora o estado de uma variável. Quando houver alteração, irá disparar uma função.
+    useEffect( () => {
+        let aData = localStorage.getItem('data'); //pega as informações que estão na Storage e armazena na variável.
+
+        //Converte o array em objeto.
+        let oData = JSON.parse(aData);
+        console.log(oData);
+        console.log(repositories);
+
+        //Armazena o objeto no repositório de nomes
+        setRepositories(oData);
+
+        localStorage.clear();
+
+    }, []); //Se deixar os [] vazio, ao renderizar a página, a função será executada.
+
     return (
-        <>
+        <S.Container>
             <S.Title>Repositório</S.Title>
             <S.List>
-                <S.ListItemBG>Repo 01</S.ListItemBG>
-                <S.ListItemBG>Repo 02</S.ListItemBG>
-                <S.ListItemBG>Repo 03</S.ListItemBG>
-                <S.ListItemBG>Repo 04</S.ListItemBG>
-                <S.ListItemBG>Repo 05</S.ListItemBG>
-                <S.ListItemBG>Repo 06</S.ListItemBG>
-                <S.ListItemBG>Repo 07</S.ListItemBG>
-                <S.ListItemBG>Repo 08</S.ListItemBG>
-                <S.ListItemBG>Repo 09</S.ListItemBG>
-                <S.ListItemBG>Repo 10</S.ListItemBG>
+                { repositories.map( repository => {
+                    return (
+                        <S.ListItemBG>Repositório: { repository }</S.ListItemBG>
+                    )
+                }) }
             </S.List>
-        </>
-    );
+        </S.Container>
+    )
 }
